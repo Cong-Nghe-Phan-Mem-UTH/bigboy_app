3aa322db8343976e11f19dd1b5a858548edb398d
dkhoagn <khoand0244@ut.edu.vn>
2026-02-02 12:59:23 +0700

    auth

--- bigboy_app_hoanchinh-main/backend/app/api/routes/auth_routes.py
+++ bigboy_app_hoanchinh-main/backend/app/api/routes/auth_routes.py
@@ -14,7 +14,6 @@ from datetime import datetime, timedelta
 
 auth_bp = Blueprint("auth", __name__)
 
-
 @auth_bp.route("/register", methods=["POST"])
 def register():
     """Register a new restaurant (tenant)"""
